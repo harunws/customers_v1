cf1201c6a926e8a688a4929b03b35c92c107415f
harunws <harunws33@gmail.com>
2023-03-07 22:27:14 +0100

    customer_create.php

---
+++ backend/customer_create.php
@@ -0,0 +1,41 @@
+<?php 
+
+  require "customer_dbconnection";
+
+  $data = json_decode(file_get_contents("php://inputs"));
+
+  if (isset($data->customer_name)
+     && isset($data->customer_email)
+     && !empty(trim($data->customer_name))
+     && !empty(trim($data->customer_email))) 
+
+  {
+    $customer_name = mysqli_real_escape_string($db_conn, trim($data->customer_name));
+    $customer_email = mysqli_real_escape_string($db_conn, trim($data->customer_email));
+    $customer_date = date('Y-m-d');
+
+    $create = mysqli_query($db_conn, " INSERT INTO customers (customer_name, customer_email, customer_date) VALUES ('$customer_name','$customer_email','$customer_date')");
+
+    if($create)
+    {
+      $last_id = mysqli_insert_id($db_conn);
+      echo json_encode(["success"=>true, "insertid"=>$last_id]);
+
+      return;
+    } 
+    else 
+    {
+      echo json_encode(["success"=>false,"msg"=>"Server Problem. Please Try Again"]);
+
+      return;
+    }
+
+  } 
+  else 
+  {
+
+    echo json_encode(["success"=>false, "msg"=>"Please fill all the reuired fields"]);
+
+    return;
+  }
+?>
